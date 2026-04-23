746d793529785e000b8a7195ae308916ecad2db3
MauricioAguas <62766009+MauricioAguas@users.noreply.github.com>
2026-04-23 14:45:16 -0500

    Actualizacion de Grupo.h e implementacion de Grupo.cpp

--- src/Grupo.h
+++ src/Grupo.h
@@ -2,11 +2,19 @@
 #define GRUPO_H
 
 #include "Equipo.h"
+#include "Partido.h"
+#include <iostream>
+using namespace std;
 
 class Grupo {
 private:
     char letra;
-    int cantEquipos;
+    int cantEquipos;     // max 4
+    Equipo* equipos[4];
+    Partido* partidos[6]; // round-robin: C(4,2) = 6 partidos
+    int puntos[4];        // puntos acumulados por equipo
+    int difGoles[4];      // diferencia de goles por equipo
+    int golesFavor[4];    // goles a favor acumulados
 
 public:
     Grupo();
@@ -14,8 +22,15 @@ public:
     ~Grupo();
 
     void agregarEquipo(Equipo* e);
-    void generarTablaClasif();
-    Equipo** obtenerClasificados();
+    void configurarPartidos(string fechaBase); // genera los 6 partidos con fechas
+    void simularPartidos();                    // simula los 6 partidos
+    void generarTablaClasif();                 // calcula puntos y ordena
+    void imprimirTabla();
+    Equipo** obtenerClasificados();            // retorna arreglo de 4 equipos ordenados
+    char getLetra();
+    Equipo* getEquipo(int i);
+    int getCantEquipos();
+    int getPuntos(int i);
 };
 
 #endif
